c98d7dfe2b5bcf6f73e828132413dcc43f675f53
jasonyu17 <jason.yu171@gmail.com>
2025-02-12 18:56:25 -0500

    created beating heart feature

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "ca_valentines");
+    gtk_header_bar_set_title(header_bar, "mysample");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "ca_valentines");
+    gtk_window_set_title(window, "mysample");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
