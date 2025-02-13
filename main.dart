83c556148f5553c6c13a8958cf868e205b726530
Ryan Bouapheng <141252385+laosrb@users.noreply.github.com>
2025-02-12 19:12:51 -0500

    initial commit

---
+++ main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:confetti/confetti.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Visual Effects',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Visual Effects'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late ConfettiController _confettiController;
+
+  @override
+  void initState() {
+    super.initState();
+    _confettiController = ConfettiController(duration: const Duration(seconds: 10));
+  }
+
+  @override
+  void dispose() {
+    _confettiController.dispose();
+    super.dispose();
+  }
+
+  void _onConfettiPressed() {
+      _confettiController.play();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Push for a surprise',
+                ),
+              ElevatedButton(
+                  onPressed: _onConfettiPressed,
+                  child: Text('Press me'),
+                ),
+              ],
+            ),
+          ),
+          _buildConfetti(),
+        ],
+      ),
+    );
+  }
+
+  /// Confetti Effect
+  Widget _buildConfetti() {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: ConfettiWidget(
+        confettiController: _confettiController,
+        blastDirection: pi / 2, // Confetti shoots downward
+        emissionFrequency: 0.05,
+        numberOfParticles: 10,
+        gravity: 0.3,
+      ),
+    );
+  }
+}
